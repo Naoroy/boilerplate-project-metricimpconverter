af105dc39e041bba74c01a164be3be7cd549742d
Naoroy <fegan.pierre@gmail.com>
2020-08-19 17:42:26 +0200

    Add dotenv package Remove mongodb, not needed Add helmet for more security

--- server.js
+++ server.js
@@ -1,45 +1,48 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+var express           = require('express');
+var cors              = require('cors');
+const helmet          = require('helmet');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
+const PORT            = process.env.PORT || 3000;
 
-var app = express();
+
+const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use(function(req, res) {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
+app.listen(PORT, function () {
+  console.log('Listening on port ' + PORT );
+  console.log(process.env.NODE_ENV + ' environment');
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
@@ -47,8 +50,8 @@ app.listen(process.env.PORT || 3000, function () {
         runner.run();
       } catch(e) {
         var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
+        console.log('Tests are not valid:');
+        console.log(error);
       }
     }, 1500);
   }
